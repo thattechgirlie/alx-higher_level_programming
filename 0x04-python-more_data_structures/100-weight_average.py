7f897f4c0931fdf64ae5c36cbb514c1788ec148f
thattechgirlie <wanji.njoro1@gmail.com>
2024-01-11 21:46:40 -0800

    more data structs

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    nmb = 0
+    d = 0
+
+    for tup in my_list:
+        nmb += tup[0] * tup[1]
+        d += tup[1]
+    return (nmb / d)
