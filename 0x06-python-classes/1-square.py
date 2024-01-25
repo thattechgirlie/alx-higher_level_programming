f0707f08702ab1f8243a71a208177a9f55f5861c
thattechgirlie <wanji.njoro1@gmail.com>
2024-01-24 22:53:18 -0800

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Define  class Square"""
+"""Define class Square"""
 
 
 class Square:
     """Represent a square"""
+
+
     def __init__(self, size):
         """Initialize a new square
         Args:
