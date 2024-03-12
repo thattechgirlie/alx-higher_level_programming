6fc054999d8d3dc09e80e456e8638690c1123c0d
thattechgirlie <wanji.njoro1@gmail.com>
2024-03-12 12:45:54 -0700

    mysql tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- Create another table with multiple rows.
+-- Create second_table with attributes id, name and score.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (id`, `name`, `score`) VALUES (2, "Alex", 3);
