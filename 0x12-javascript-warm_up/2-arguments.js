a68c3b2da2f040d5f10c6c9c8bf4f2f9109d36a9
thattechgirlie <wanji.njoro1@gmail.com>
2024-04-10 20:29:38 +0300

    tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (process.argc.length === 2){
-	console.log('No Argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
-} else {
-	console.log('Arguments found');
-}
+if (process.argc.length === 2) { console.log('No Argument'); } else if (process.argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found');}
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-	console.log('No Argument');
-} else {
-	console.log(process.argv[2]);
-}
+if (process.argv[2] === undefined) { console.log('No Argument');} else { console.log(process.argv[2]); }
