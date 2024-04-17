b2751ec40ce2de11444d3c743eb30626ac5f7417
thattechgirlie <wanji.njoro1@gmail.com>
2024-04-17 05:03:44 -0700

    corrections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,7 @@ script should take 4 arguments
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states""")
