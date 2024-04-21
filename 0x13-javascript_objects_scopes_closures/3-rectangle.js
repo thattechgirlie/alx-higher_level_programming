66f1b5c996b61f46ab531b887676f0e3520f6708
thattechgirlie <wanji.njoro1@gmail.com>
2024-04-21 18:41:58 +0300

    tasks

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor (w, h) {
-		if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+   }
 
-	print () {
-		for (let a = 0; a < this.height; a++) {
-			console.log('X'.repeat(this.width));
-		}
-	}
+  print () {
+    for (let a = 0; a < this.height; a++) {
+      console.log('X'.repeat(this.width));
+    }
+   }
+  };
